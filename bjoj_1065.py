c83d5aa0f27a8f0dbb81845c10c4371114e2efc2
basekim14 <basekim14@gmail.com>
2020-06-28 23:09:37 +0900

    Study 0628

---
+++ bjoj_1065.py
@@ -0,0 +1,6 @@
+"""
+ㄱㄱㅊ <basekim14@gmail.com>, 20-06-26
+Baekjoon Online Judge Study - 1065()
+"""
+
+from sys import stdin
---
+++ bjoj_11654.py
@@ -0,0 +1,8 @@
+"""
+ㄱㄱㅊ <basekim14@gmail.com>, 20-06-28
+Baekjoon Online Judge Study - 10654(ASCII code)
+"""
+
+from sys import stdin
+
+print(ord(stdin.readline().strip()))
