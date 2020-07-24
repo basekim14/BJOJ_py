fdc9db0cfc0591f7e41276c8618a97c377adea5c
basekim14 <basekim14@gmail.com>
2020-07-24 16:52:12 +0900

    Study 0724

---
+++ bjoj_4948.py
@@ -0,0 +1,34 @@
+"""
+ㄱㄱㅊ <basekim14@gmail.com>, 20-07-24
+Baekjoon Online Judge Study - 4948(bertrand's postulate)
+"""
+n = 2 * 123456
+sieve = [False] + [True] * n
+sieve[1] = False
+m = n
+for i in range(2, m+1):
+    if sieve[i] == True:
+        for j in range(i+i, m+1, i):
+            sieve[j] = False
+
+while True:
+    t = int(input())
+    if t == 0:
+        break
+    elif t == 1:
+        print(1)
+    else:
+        print(sum(sieve[t + 1:2 * t + 1]))
+
+"""
+# runtime error
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    elif n == 1:
+        print(1)
+    else:
+        numlist = range(n + 1, 2 * n + 1)
+        print(sum(map(lambda x: all(x % j for j in range(2, x)), numlist)))
+"""
