e3cb993dddd1ec0f50e6c87c71b9e938ca5a1ad7
basekim14 <basekim14@gmail.com>
2020-07-29 01:55:18 +0900

    Study 0729

---
+++ bjoj_3053.py
@@ -0,0 +1,10 @@
+"""
+ㄱㄱㅊ <basekim14@gmail.com>, 20-07-29
+Baekjoon Online Judge Study - 3053(taxi cab geometry)
+"""
+
+from math import pi
+
+R = int(input())
+print("%.4f" % (R * R * pi))
+print("%.4f" % (R * R * 2))
