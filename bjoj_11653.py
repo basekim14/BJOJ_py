fc34bb774e0ecb22aa851e058995dc59016f4d27
basekim14 <basekim14@gmail.com>
2020-08-03 19:27:17 +0900

    Study 0803

---
+++ bjoj_11653.py
@@ -0,0 +1,16 @@
+"""
+ㄱㄱㅊ <basekim14@gmail.com>, 20-08-03
+Baekjoon Online Judge Study - 1037(11653)
+"""
+
+N = int(input())
+i = 2
+
+while i <= int(N**0.5):
+    if N % i == 0:
+        N //= i
+        print(i)
+    else:
+        i += 1
+if N > 1:
+    print(N)
